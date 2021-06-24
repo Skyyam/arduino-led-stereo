1a7d3a6ceb3050b8510e0c20b402d9d62eb17c8d
Tina Rickard <silverwater101@gmail.com>
2021-06-24 13:56:07 -0700

    add initial (example) code

--- src/led_stereo_sketch/led_stereo_sketch.ino
+++ src/led_stereo_sketch/led_stereo_sketch.ino
@@ -4,12 +4,48 @@
  * @Tina Rickard
  */
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
 
+#define PIN 6
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(
+  5,
+  8,
+  PIN,
+  NEO_MATRIX_TOP
+    + NEO_MATRIX_RIGHT
+    + NEO_MATRIX_COLUMNS
+    + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB + NEO_KHZ800
+);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(colors[0]);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int x = matrix.width();
+int pass = 0;
 
+void loop() {
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  matrix.print(F("Howdy"));
+  if(--x < -36) {
+    x = matrix.width();
+    if(++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
+  delay(100);
 }
